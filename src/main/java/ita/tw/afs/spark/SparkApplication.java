c7c491953a1d6c282863318a91e5b6f113b53a38
cruztr <cruztr@oocl.com>
2019-10-24 21:00:45 +0800

    - "revert test failing build"

--- src/main/java/ita/tw/afs/spark/SparkApplication.java
+++ src/main/java/ita/tw/afs/spark/SparkApplication.java
@@ -1,6 +1,6 @@
 package ita.tw.afs.spark;
 
-//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
