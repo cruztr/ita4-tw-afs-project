79144d0d2c2a97aa7ee62440638d11cb7f680d8f
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-27 16:22:23 +0800

    [VIEW RESERVATION] - Removed unused imports

--- src/main/java/ita/tw/afs/spark/service/CarOwnerService.java
+++ src/main/java/ita/tw/afs/spark/service/CarOwnerService.java
@@ -1,16 +1,12 @@
 package ita.tw.afs.spark.service;
 
-import ita.tw.afs.spark.model.CarOwner;
 import ita.tw.afs.spark.model.Reservation;
 import ita.tw.afs.spark.repository.CarOwnerRepository;
 import ita.tw.afs.spark.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 
 @Service
 public class CarOwnerService {
