b201fd886ab41cfd5f1e1c502df99b99b8fc2d53
Mikecrophones07 <mike.toradora07@gmail.com>
2019-10-27 17:01:35 +0800

    [Login-App_User] add crossOrigin in controller

--- src/main/java/ita/tw/afs/spark/repository/CarOwnerRepository.java
+++ src/main/java/ita/tw/afs/spark/repository/CarOwnerRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarOwnerRepository extends JpaRepository<CarOwner, Long> {
+
     CarOwner findByUsernameAndPassword(String username, String password);
 }
